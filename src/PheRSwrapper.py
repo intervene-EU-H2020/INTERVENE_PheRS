c3865e64c076d7a0ae85a0262d8f1758fb7f37b9
Kira Detrois <kira.detrois@helsinki.fi>
2024-05-10 12:44:51 +0300

    PheCode truncation and additional option for ICD-code level used
    - Truncating phecodes i.e. 608.3 and 608.2 to 608
    - Created functions in PheRS preprocessing
    - More detailed output for the excluded phecodes
    - Excluded phecodes are now also always based the overarching category. i.e. cant just exclude 608.2 and not 608.3 and all other subcategories.
    - Added choice if wanting to use only primary or also secondary ICDs and whether they should be weighed differently
    - New output file with case and control counts for each endpoint for train and test set
    - Added scoring informationalso for train set
    - Added prediction file for all individuals not just the test set

--- src/PheRSwrapper.py
+++ src/PheRSwrapper.py
@@ -19,7 +19,6 @@ def PheRSwrapper():
     parser.add_argument("--targetphenotype",help="Name of the target phenotype.",type=str,default=None)
     parser.add_argument("--excludephecodes",help="Path to a file containing the Phecode(s) corresponding to and related to the target phenotype that should be excluded from the analysis.",type=str,default=None)
     parser.add_argument("--outdir",help="Output directory (must exist, default=./).",type=str,default="./")
-    parser.add_argument("--model",help="Type of PheRS model fitted, default=elasticnet.",type=str,choices=['elasticnet','gradboost'],default='elasticnet')
     parser.add_argument("--testfraction",help="Fraction of IDs used in test set (default=0.15). Sampling at random.",type=str,default='0.15')
     parser.add_argument("--testidfile",help="Full path to a file containing the IDs used in the test set. If given, overrides --testfraction.",type=str,default=None)
     parser.add_argument("--washout_window",help="Start and end dates for washout (default= 2009-01-01 2010-12-31).",type=str,nargs=2,default=['2009-01-01','2010-12-31'])
@@ -35,8 +34,11 @@ def PheRSwrapper():
     parser.add_argument("--controlfraction",help="How many controls to use, number of cases multiplied by this flag value (default=None, meaning all controls are used).",type=str,default=None)
     parser.add_argument("--nproc",help="Number of parallel processes used (default=1).",type=str,default='1')
     parser.add_argument("--paramgridfile",help="File containing the parameter grid explored using cross-validation. One parameter per row, first value is key, rest are possible values.",type=str,default=None)
-    
+    parser.add_argument("--ICD_levels",help="Level of ICD-codes to use. 1=primary, 2=primary+secondary.",type=int,default=1)
+    parser.add_argument("--secondary_ICD_weight",help="How much secondary ICDs should weigh compared to primary",type=float,default=1)
+
     args = parser.parse_args()
+    print(args.targetphenotype)
 
     #save the command used to evoke this script into a file
     logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename=args.outdir+'PheRSwrapper.log',level=logging.INFO,filemode='w')
@@ -55,7 +57,8 @@ def PheRSwrapper():
     includevars = ''
     for i in args.includevars: includevars += ' '+i
     
-    cmd = 'PheRS_preprocess.py --ICDfile '+args.ICDfile+' --phenotypefile '+args.phenotypefile+' --phecodefile '+args.phecodefile+' --ICD9tophecodefile '+args.ICD9tophecodefile+' --ICD10tophecodefile '+args.ICD10tophecodefile+' --ICD10CMtophecodefile '+args.ICD10CMtophecodefile+' --targetphenotype '+args.targetphenotype+' --excludephecodes '+args.excludephecodes+' --outdir '+outdir_i+' --washout_window '+args.washout_window[0]+' '+args.washout_window[1]+' --exposure_window '+args.exposure_window[0]+' '+args.exposure_window[1]+' --observation_window '+args.observation_window[0]+' '+args.observation_window[1]+' --seed '+args.seed+' --nproc '+args.nproc+' --testfraction '+args.testfraction+' --frequency '+args.frequency+' --maxage '+str(args.maxage)+' --minage '+str(args.minage)
+    cmd = 'python3 /scratch/project_2007428/users/detrois/scripts/INTERVENE_PheRS/src/PheRS_preprocess.py --ICDfile '+args.ICDfile+' --phenotypefile '+args.phenotypefile+' --phecodefile '+args.phecodefile+' --ICD9tophecodefile '+args.ICD9tophecodefile+' --ICD10tophecodefile '+args.ICD10tophecodefile+' --ICD10CMtophecodefile '+args.ICD10CMtophecodefile+' --targetphenotype '+args.targetphenotype+' --excludephecodes '+args.excludephecodes+' --outdir '+outdir_i+' --washout_window '+args.washout_window[0]+' '+args.washout_window[1]+' --exposure_window '+args.exposure_window[0]+' '+args.exposure_window[1]+' --observation_window '+args.observation_window[0]+' '+args.observation_window[1]+' --seed '+args.seed+' --nproc '+args.nproc+' --testfraction '+args.testfraction+' --frequency '+args.frequency+' --maxage '+str(args.maxage)+' --minage '+str(args.minage) + ' --ICD_levels '+str(args.ICD_levels) + ' --secondary_ICD_weight '+str(args.secondary_ICD_weight)
+    
     if args.controlfraction!=None: cmd += ' --controlfraction '+args.controlfraction
     if args.excludeICDfile!=None: cmd += ' --excludeICDfile '+args.excludeICDfile
     if args.testidfile!=None: cmd += ' --testidfile '+args.testidfile
@@ -65,21 +68,15 @@ def PheRSwrapper():
 
     print('Preprocessing done.')
     #run PheRS fitting
-    if args.model=='elasticnet':
-        cmd = 'fitLogreg.py --infile '+outdir_i+'target-'+args.targetphenotype+'-PheRS-ML-input.txt.gz --excludevars '+outdir_i+'target-'+args.targetphenotype+'-excluded-phecodes.txt --paramgridfile '+args.paramgridfile+' --outdir '+outdir_i+' --nproc '+args.nproc+' --scoring neg_log_loss'
-    elif args.model=='gradboost':
-        cmd = 'fitGradBoost.py --infile '+outdir_i+'target-'+args.targetphenotype+'-PheRS-ML-input.txt.gz --excludevars '+outdir_i+'target-'+args.targetphenotype+'-excluded-phecodes.txt --paramgridfile '+args.paramgridfile+' --outdir '+outdir_i+' --nproc '+args.nproc+' --scoring neg_log_loss'
-        
+    cmd = 'python3 /scratch/project_2007428/users/detrois/scripts/INTERVENE_PheRS/src/fitLogreg.py --infile '+outdir_i+'target-'+args.targetphenotype+'-PheRS-ML-input.txt.gz --excludevars '+outdir_i+'target-'+args.targetphenotype+'-excluded-phecodes.txt --paramgridfile '+args.paramgridfile+' --outdir '+outdir_i+' --nproc '+args.nproc+' --scoring neg_log_loss'
     logging.info(cmd)
     print('Starting to fit...')
     os.system(cmd)
 
     #score the fitted model
-    if args.model=='elasticnet':
-        cmd = 'scoreLogreg.py --infile '+outdir_i+'target-'+args.targetphenotype+'-PheRS-ML-input.txt.gz --outdir '+outdir_i+' --scaler '+outdir_i+'scaler.pkl --imputer '+outdir_i+'imputer.pkl --excludevars '+outdir_i+'target-'+args.targetphenotype+'-excluded-phecodes.txt --model '+outdir_i+'best_model.pkl'
-    elif args.model=='gradboost':
-        cmd = 'scoreGradBoost.py --infile '+outdir_i+'target-'+args.targetphenotype+'-PheRS-ML-input.txt.gz --outdir '+outdir_i+' --scaler '+outdir_i+'scaler.pkl --excludevars '+outdir_i+'target-'+args.targetphenotype+'-excluded-phecodes.txt --model '+outdir_i+'best_model.pkl'
+    cmd = 'python3 /scratch/project_2007428/users/detrois/scripts/INTERVENE_PheRS/src/scoreLogreg.py --infile '+outdir_i+'target-'+args.targetphenotype+'-PheRS-ML-input.txt.gz --outdir '+outdir_i+' --scaler '+outdir_i+'scaler.pkl --imputer '+outdir_i+'imputer.pkl --excludevars '+outdir_i+'target-'+args.targetphenotype+'-excluded-phecodes.txt --model '+outdir_i+'best_model.pkl'
     logging.info(cmd)
     os.system(cmd)
+
         
 PheRSwrapper()
